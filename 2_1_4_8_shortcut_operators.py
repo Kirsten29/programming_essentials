2c896db8bac195e8a6f37bb2a6c1e2b267ecf3c6
Kirsten Ferwerda <kirstenferwerda@hotmail.com>
2020-09-28 15:03:13 +0200

    Add files

---
+++ 2_1_4_8_shortcut_operators.py
@@ -0,0 +1,10 @@
+x = x * 2 -> x *= 2
+sheep = sheep + 1 -> sheep += 1
+variable = variable op expression
+variable op= expression
+
+i = i + 2 * j -> i += 2 * j
+var = var / 2 -> var/= 2
+rem = rem % 10 -> rem %= 10
+j = j - (i + var + rem) -> j -= (i + var + rem)
+x = x ** 2 -> x **= 2
