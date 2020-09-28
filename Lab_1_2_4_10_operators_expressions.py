170a4117cd18f09b9bf3d1d26ebc4fa643707d83
Kirsten Ferwerda <kirstenferwerda@hotmail.com>
2020-09-28 18:30:18 +0200

    Add files

---
+++ Lab_1_2_4_10_operators_expressions.py
@@ -0,0 +1,26 @@
+#Your task is to complete the code in order to evaluate the following expression:
+# 3x3 - 2x2 + 3x - 1
+
+x =  # hardcode your test data here
+x = float(x)
+# write your code here
+print("y =", y)
+Test data sample input
+x = 0
+x = 1
+x = -1
+#Solution:
+x = 0
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print("y =", y)
+
+x = 1
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print("y =", y)
+
+x = -1
+x = float(x)
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+print("y =", y)
