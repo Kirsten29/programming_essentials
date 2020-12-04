e089d0780a7ba3c6623b68129aaf15e8ee4621be
Kirsten Ferwerda <kirstenferwerda@hotmail.com>
2020-12-04 20:43:49 +0100

    Add files

---
+++ Module_2/LAB_2_1_1_20_arrow.py
@@ -0,0 +1,51 @@
+"Minimize the number of print() function invocations by inserting 
+"the \n sequence into the strings.
+"Make the arrow twice as large (but keep the proportions)
+"Duplicate the arrow, placing both arrows side by side; note: a string 
+"may be multiplied by using the following trick: "string" * 2 will produce 
+"stringstring" (we'll tell you more about it soon)
+"Remove any of the quotes, and look carefully at Python's response; pay attention to where Python sees an error - is this the place where the error really exists?
+"Do the same with some of the parentheses;
+"Change any of the print words into something else, differing only in case (e.g., Print) - what happens now?
+"Replace some of the quotes with apostrophes; watch what happens carefully.
+
+
+
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+
+
+print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****")
+
+
+print("        **")
+print("       ****")
+print("      **  **")
+print("     **    **")
+print("    **      **")
+print("   **        **")
+print("  **          **")
+print(" **            **")
+print("***            ***")
+print("  **          **")
+print("  **          **")
+print("  **          **")
+print("  **          **")
+print("  **          **")
+print("  **************")
+print("  **************")
+
+print("    *    "*2)
+print("   * *   "*2)
+print("  *   *  "*2)
+print(" *     * "*2)
+print("***   ***"*2)
+print("  *   *  "*2)
+print("  *   *  "*2)
+print("  *****  "*2)
