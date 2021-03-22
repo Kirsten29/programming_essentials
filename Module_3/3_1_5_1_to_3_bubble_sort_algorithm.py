9ea5ca88e2d84416cfd7eccf671db076889f704e
Kirsten Ferwerda <kirstenferwerda@hotmail.com>
2021-03-22 12:28:38 +0100

    Add file

--- Module_3/3_1_5_1_to_3_bubble_sort_algorithm.py
+++ Module_3/3_1_5_1_to_3_bubble_sort_algorithm.py
@@ -1,13 +1,13 @@
-3_1_5_1
--------
-3_1_5_2
+#3_1_5_1
+#-------
+#3_1_5_2
 myList = [8, 10, 6, 2, 4] # list to sort
 
 for i in range(len(myList) - 1): # we need (5 - 1) comparisons
     if myList[i] > myList[i + 1]: # compare adjacent elements
         myList[i], myList[i + 1] = myList[i + 1], myList[i] # if we end up here 
         #it means that we have to swap the elements
----
+#---
 myList = [8, 10, 6, 2, 4] # list to sort
 swapped = True # it's a little fake - we need it to enter the while loop
 
@@ -19,15 +19,15 @@ while swapped:
             myList[i], myList[i + 1] = myList[i + 1], myList[i]
 
 print(myList)
----
-3_1_5_3
+#---
+#3_1_5_3
 myList = [8, 10, 6, 2, 4]
 myList.sort()
 print(myList)
-The snippet's output is as follows:
+#The snippet's output is as follows:
 
-[2, 4, 6, 8, 10]
----
+#[2, 4, 6, 8, 10]
+#---
 myList = []
 swapped = True
 num = int(input("How many elements do you want to sort: "))
